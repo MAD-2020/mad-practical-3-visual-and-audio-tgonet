d9153724e7b683a8e378894a7630d56d3b7e7fe2
Ming Zhe Tan <s10193694@connect.np.edu.sg>
2020-05-08 12:25:04 +0800

    IT WORKED!

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,32 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer media;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+
+        //once done with the playback
+        if(!media.isPlaying()){
+
+            media.release();
+            media = null;
+        }
+
+        VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(
+              Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        view.start();
     }
 }
